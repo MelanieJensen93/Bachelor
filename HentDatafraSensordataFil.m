762a3454dd7437c121951bd73dfdd2546918b2b3
MelanieJensen93 <melanie-jensen93@hotmail.com>
2018-10-12 14:25:41 +0200

    Funktion hent data fra sensorfil er oprettet

---
+++ HentDatafraSensordataFil.m
@@ -0,0 +1,6 @@
+function [outputArg1,outputArg2] = HentDatafraSensordataFil(inputArg1,inputArg2)
+%HENTDATAFRASENSORDATAFIL Summary of this function goes here
+%   Detailed explanation goes here
+
+end
+
